39cc1d0a5c7437d8d41c659ad2dd95303b4e97ed
Brandon Fannin <bfannin13@gmail.com>
2020-06-03 14:02:26 -0400

    implement read through on click functionality

--- content.js
+++ content.js
@@ -1,6 +1,14 @@
+const APP_TIMEOUT_MS = 50;
+const timer = (waitTime) => 
+  new Promise(resolve => setTimeout(resolve, waitTime ));
+var isReading = false;
+
 document.addEventListener('click', function(e) { 
-  console.log('directed-reading extension activated')
-  beginReading(e.target);
+  console.log('directed-reading extension activated')  
+  isReading = !isReading;
+  if (isReading) {
+    beginReading(e.target);
+  }
 });
 
 /**
@@ -10,14 +18,14 @@ document.addEventListener('click', function(e) {
  * 
  * @param {HTMLElement} target element user clicks to be read
  */
-function beginReading(target) {
-  while (target) {
+async function beginReading(target) {
+  while (target && isReading) {
     // console.log for dev/debugging
     console.log('beginReading function called: ')
     console.log(target)
 
     // read through the text
-    doChunkRead(target);
+    await doChunkRead(target);
 
     // update the value of target
     // to be its next sibling 
@@ -29,8 +37,45 @@ function beginReading(target) {
   }
 }
 
+  // loop through the text, one word at
+  // a time, moving the <span> tag to 
+  // highlight 
 function doChunkRead(elem) {
-  if (elem.style) {
-    elem.style.color="red";
+  if (!elem.textContent) { return }
+
+  const words = elem.textContent.split(" ");
+  
+  // initialize recursive highlight function
+  focusWord(words, 0, elem);
+  return timer(APP_TIMEOUT_MS * words.length + 1)
+}
+
+async function focusWord(words, word, elem) {
+  // if done reading the section
+  // or the user has clicked again
+  if (word > words.length - 1 || !isReading) { 
+    elem.innerHTML = words.join(" ");
+    return 
   }
+
+  elem.innerHTML = renderHTML(words, word);
+
+  await timer(APP_TIMEOUT_MS);
+
+  focusWord(words, word + 1, elem);
+}
+
+function renderHTML(words, word) {
+  const chunkA = words
+                .filter( (v, i) => i < word )
+                .join(" ");
+  const chunkB = words
+                .filter( (v, i) => i > word )
+                .join(" ");
+
+  return `${chunkA} 
+          <span style="background-color: yellow; border-radius: 3px;">
+          ${words[word]}
+          </span>
+          ${chunkB}`;
 }
