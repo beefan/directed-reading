6058374f5d9b84c17c50f919f515891e491a0d79
Brandon Fannin <bfannin13@gmail.com>
2020-06-03 12:42:48 -0400

    navigate the dom to highlight all text

--- content.js
+++ content.js
@@ -1,4 +1,36 @@
 document.addEventListener('click', function(e) { 
-  console.log('logging')
-  e.target.style.color="red"
+  console.log('directed-reading extension activated')
+  beginReading(e.target);
 });
+
+/**
+ * Tries to start reading text inside HTMLElement
+ * Reads through all text and then nextSibling
+ * text in a loop until there is no nextSibling
+ * 
+ * @param {HTMLElement} target element user clicks to be read
+ */
+function beginReading(target) {
+  while (target) {
+    // console.log for dev/debugging
+    console.log('beginReading function called: ')
+    console.log(target)
+
+    // read through the text
+    doChunkRead(target);
+
+    // update the value of target
+    // to be its next sibling 
+    if (!target.nextSibling && target.parent) {
+      target = target.parent.nextSibling;
+    } else {
+      target = target.nextSibling;
+    }
+  }
+}
+
+function doChunkRead(elem) {
+  if (elem.style) {
+    elem.style.color="red";
+  }
+}
